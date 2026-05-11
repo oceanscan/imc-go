0f60c5cc91054f8f04fb59fa2e7f322ab477f982
Jose Pinto <zepinto@gmail.com>
2026-05-11 11:38:58 +0100

    parse_lsf: accept CLI args and support gzip-compressed inputs
    Replace hardcoded data/test1 paths with command-line arguments and
    transparently decompress .gz files for both IMC.xml and the .lsf log.

--- cmd/parse_lsf/main.go
+++ cmd/parse_lsf/main.go
@@ -1,27 +1,66 @@
 package main
 
 import (
+	"compress/gzip"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/oceanscan/imc-go"
 )
 
+type gzReadCloser struct {
+	*gzip.Reader
+	f *os.File
+}
+
+func (g *gzReadCloser) Close() error {
+	err := g.Reader.Close()
+	if cerr := g.f.Close(); err == nil {
+		err = cerr
+	}
+	return err
+}
+
+// openMaybeGz opens a file, transparently decompressing it if the path ends in .gz.
+func openMaybeGz(path string) (io.ReadCloser, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	if !strings.HasSuffix(path, ".gz") {
+		return f, nil
+	}
+	gz, err := gzip.NewReader(f)
+	if err != nil {
+		f.Close()
+		return nil, err
+	}
+	return &gzReadCloser{Reader: gz, f: f}, nil
+}
+
 func main() {
-	xmlPath := "data/test1/IMC.xml"
-	dataPath := "data/test1/Data.lsf"
+	if len(os.Args) != 3 {
+		log.Fatalf("usage: %s <IMC.xml[.gz]> <Data.lsf[.gz]>", os.Args[0])
+	}
+	xmlPath, dataPath := os.Args[1], os.Args[2]
 
 	fmt.Printf("Loading protocol from %s...\n", xmlPath)
-	xmlProto, err := imc.ParseXML(xmlPath)
+	xmlReader, err := openMaybeGz(xmlPath)
+	if err != nil {
+		log.Fatalf("Failed to open IMC.xml: %v", err)
+	}
+	xmlProto, err := imc.ParseReader(xmlReader)
+	xmlReader.Close()
 	if err != nil {
 		log.Fatalf("Failed to parse IMC.xml: %v", err)
 	}
 	proto := imc.NewProtocol(xmlProto)
 
 	fmt.Printf("Opening data file %s...\n", dataPath)
-	f, err := os.Open(dataPath)
+	f, err := openMaybeGz(dataPath)
 	if err != nil {
 		log.Fatalf("Failed to open data file: %v", err)
 	}
